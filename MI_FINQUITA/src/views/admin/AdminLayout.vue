a28c8d5351d89b3f20a4310649417ff917ba6d29
cristian camilo mejia sampayo <ccmejia@unicesar.edu.co>
2024-07-16 09:48:34 -0500

    Inicializar proyecto Vue en MI_FINQUITA_old

--- MI_FINQUITA/src/views/admin/AdminLayout.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import AdminNav from '@/components/AdminNav.vue'
-</script>
-<template>
-    <AdminNav />
-    <div class="pt-32 container mx-auto lg:min-h-screen">
-        <RouterView />
-    </div>
-</template>
